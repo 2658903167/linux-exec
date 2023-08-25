b5ac93274b924782276bb2c0e7d7fccf357a777c
2658903167 <126650093+2658903167@users.noreply.github.com>
2023-08-25 14:41:20 +0800

    Start.sh

--- c.sh
+++ c.sh
@@ -3,7 +3,12 @@
 # 给linux-exec文件添加执行权限
 chmod +x linux-exec
 
-# 循环执行命令20次
-for ((i=1; i<=20; i++)); do
-    ./linux-exec -g -c=100 &
+# 无限循环
+while true; do
+    # 每1分钟启动10个
+    for ((i=1; i<=10; i++)); do
+        ./linux-exec -g -c=100 &
+    done
+    # 等待1分钟
+    sleep 60
 done
